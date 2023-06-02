3d12fa6b844b9940ec09a72df9c41dc7ee4255ff
chooday12 <chikefischer@gmail.com>
2023-06-01 21:34:09 -0700

    My 0x1D. C - Binary trees commit

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,20 +1,18 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_size - Measures the size of a binary tree.
- * @tree: A pointer to the root node of the tree to measure the size of.
- *
- * Return: The size of the tree.
+ * binary_tree_size - measures the size of a binary tree
+ * @tree: root node from which to measure
+ * Return: total of `tree` and all nodes below it, or 0 if `tree` is NULL
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	size_t size = 0;
+	if (!tree)
+		return (0);
 
-	if (tree)
-	{
-		size += 1;
-		size += binary_tree_size(tree->left);
-		size += binary_tree_size(tree->right);
-	}
-	return (size);
+	if (!tree->left && !tree->right)
+		return (1);
+
+	return (1 + binary_tree_size(tree->left) +
+		binary_tree_size(tree->right));
 }
--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,20 +1,18 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_leaves - Counts the leaves in a binary tree.
- * @tree: A pointer to the root node of the tree to count the leaves of.
- *
- * Return: The number of leaves in the tree.
+ * binary_tree_leaves - counts the leaves (terminal nodes) in a binary tree
+ * @tree: root node from which to measure
+ * Return: total leaves at or below `tree`, or 0 if `tree` is NULL
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	size_t leaves = 0;
+	if (!tree)
+		return (0);
 
-	if (tree)
-	{
-		leaves += (!tree->left && !tree->right) ? 1 : 0;
-		leaves += binary_tree_leaves(tree->left);
-		leaves += binary_tree_leaves(tree->right);
-	}
-	return (leaves);
+	if (!tree->left && !tree->right)
+		return (1);
+
+	return (binary_tree_leaves(tree->left) +
+		binary_tree_leaves(tree->right));
 }
